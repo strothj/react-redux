367ca6b218dda83916cb0c220f98f42e6af9e1a2
strothj <jason@jasons.io>
2017-01-25 05:25:56 -0500

    Added redux provider

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 require('babel-polyfill');
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Provider = require('react-redux').Provider;
+
+var store = require('./store');
+var RepositoryList = require('./components/repository-list');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+        <Provider store={store}>
+            <RepositoryList />
+        </Provider>,
+        document.getElementById('app')
+    );
+});
